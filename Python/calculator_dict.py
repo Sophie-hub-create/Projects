ae94c086df1bfe5fadb4e3c69af29de0c50a1952
Sophie-hub-create <s.ischenko1990@gmail.com>
2021-12-13 10:21:10 +0100

    kleine aenderungen

--- Python/calculator_dict.py
+++ Python/calculator_dict.py
@@ -1,21 +1,4 @@
-logo = """
- _____________________
-|  _________________  |
-| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
-| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
-|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
-| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
-| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
-| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
-| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
-| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
-| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
-| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
-| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
-|_____________________|
-"""
-print(logo)
-print("Welcome to the Calculator\n")
+from calculator_art import logo
 
 def add(number1, number2):
     return number1+number2
@@ -43,13 +26,29 @@ operations ={
 "*": multi,
 "/": div
 }
-number1 = int(input("Which number do you want to calculate first?  \n"))
-number2 = int(input("Which number do you want to calculate then?  \n"))
+def calculator():
+    print(logo)
+    print("Welcome to the Calculator\n")
 
-for symbol in operations:
+    number1 = float(input("Which number do you want to calculate first?\n"))
+    for symbol in operations:
         print(symbol)
-decision = input(f"Which operation would you like to calculate the numbers {number1} and {number2}? Type in '+', '-', '*' or '/' \n")
-calculating = operations [decision]
-output = calculating (number1, number2)
+    continue_calculating = True
 
-print(f"{number1} {decision} {number2} = {output}")
+    while continue_calculating == True:
+        decision = input(f"Which operation would you like to pick? Type in '+', '-', '*' or '/' \n")
+        number2 = float(input("Which number do you want to calculate then?  \n"))
+        calculating = operations [decision]
+        output = calculating(number1, number2)
+
+        print(f"{number1} {decision} {number2} = {output}")
+
+        if input(f"Type 'yes' to continue with calculating with {output}, or type 'no' to start again a new calculation: \n") == "yes":
+            number1 = output
+        else:
+            continue_calculating = False
+            calculator()
+
+calculator()           
+
+
