a491ff2f090c06fb9e4a2ed4889f05ac7a5258ad
Sophie-hub-create <s.ischenko1990@gmail.com>
2021-12-18 14:00:28 +0100

    lowerHigher

--- Python/lower_or_higher.py
+++ Python/lower_or_higher.py
@@ -1,4 +1,20 @@
+
+import random
+from lower_or_higher_data import data
 from lower_or_higher_art import *
 print(logo)
 
 print(vs)
+
+data1 = random.randint(0, len(data))
+print(data1)
+
+dict1 = data[data1]
+print(dict1)
+
+dataDict1 = dict1.get('name')+", a "+ dict1.get("description")+", from "+dict1.get("country")
+print(dataDict1)
+
+followerDataDict1 = dict1.get("follower_count")
+
+print (followerDataDict1)
