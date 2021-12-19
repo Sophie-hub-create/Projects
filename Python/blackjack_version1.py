9a585057244d21e29beb6662397ffe31647c8841
Sophie-hub-create <s.ischenko1990@gmail.com>
2021-12-19 10:45:58 +0100

    blackjack

--- Python/blackjack_version1.py
+++ Python/blackjack_version1.py
@@ -1,5 +1,5 @@
 import random
-from Python.blackjack_art import logo
+from blackjack_art import logo
 
 cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
@@ -7,36 +7,26 @@ def selectRandom():
     result_rand = random.choice(cards)
     return result_rand
 
-print(logo)
-print("Welcome to BlackJack\n")
-
-beginCountDealer = 0
-beginCountDealer += selectRandom()
-
-print(f"Dealer has {beginCountDealer}\n")
-
 def cards_player():
     player_count= 0
-    card1_player = selectRandom()
-    print(f"You have got {card1_player}\n")
-    player_count += card1_player
+    player_count += beginCountPlayer
+    
     again = True
     while again:
         getCard = input("Do you want get one more card? Type 'y' or 'n'\n")
         if getCard == 'y':
             again = True
-            card2_player = selectRandom()
-            print(f"\nYou get a {card2_player}\n")
-            player_count+= card2_player
+            card3_player = selectRandom()
+            if selectRandom() == 11 and player_count >11 :
+                card3_player ==1
+            print(f"\nYou get a {card3_player}")
+            player_count+= card3_player
             print(f"You have got {player_count} on the hand\n")
         else:
             again = False
-            return player_count         
-               
+            return player_count
     return player_count
-        
-    
-
+            
 def cards_dealer():
     dealer_count = 0
     dealer_count += beginCountDealer   
@@ -50,15 +40,36 @@ def cards_dealer():
             return result
     return dealer_count
 
+def playerOrDealer(score_dealer, score_player):
+    
+    if score_dealer > 21 and score_player > 21:
+        print(f"\nDealer has {score_dealer}, Player has {score_player}\n Dealer WINS, Player lost.")
+    elif score_dealer > 21 and score_player <= 21:
+        print(f"\nDealer has {score_dealer}, Player has {score_player}\nDealer Bust, Player WINS")
+    elif score_dealer <=21 and score_player > 21:
+        print(f"\nDealer has {score_dealer}, Player has {score_player}\n Dealer WINS, Player Bust")
+    elif score_dealer < 21 and score_player < 21 and score_dealer==score_player:
+        print(f"\nDealer has {score_dealer}, Player has {score_player}\nNo one wins.")
+    elif score_dealer < 21 and score_player < 21 and score_dealer > score_player:
+            print(f"\nDealer has {score_dealer}, Player has {score_player}\nDealer WINS, Player Bust")
+    elif score_dealer < 21 and score_player < 21 and score_dealer < score_player:
+            print(f"\nDealer has {score_dealer}, Player has {score_player}\nDealer Bust, Player WINS")
+
+print(logo)
+print("Welcome to BlackJack\n")
+beginCountDealer = 0
+beginCountDealer += selectRandom()
+print(f"Dealer has {beginCountDealer}\n")
+beginCountPlayer = 0
+beginCountPlayer += selectRandom()
+print(f"You get a {beginCountPlayer}")
+a = selectRandom()
+beginCountPlayer += a
+print(f"You get a {a}, and have {beginCountPlayer} on the hand")
 score_dealer = cards_dealer()
 score_player = cards_player()
+playerOrDealer(score_dealer, score_player)
 
-if score_dealer > 21 and score_player > 21:
-    print(f"\nDealer has {score_dealer}, Player has {score_player}\n Dealer WINS, Player lost.")
-elif score_dealer > 21 and score_player <=21:
-    print(f"\nDealer has {score_dealer}, Player has {score_player}\nDealer Bust, Player WINS")
-elif score_dealer <=21 and score_player >21:
-    print(f"\nDealer has {score_dealer}, Player has {score_player}\n Dealer WINS, Player Bust")
 
 
 
