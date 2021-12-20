55504af7928a16d81964451cf6ccdd1517eff5c5
Sophie-hub-create <s.ischenko1990@gmail.com>
2021-12-20 16:19:41 +0100

    finished caffe machine

--- Python/coffe_machine.py
+++ Python/coffe_machine.py
@@ -46,6 +46,9 @@ def report():
     print(f"Milk: {dataMilk}ml")
     print(f"Coffee: {dataCoffee}g")
     print(f"Money: ${money}")
+
+
+x = ['espresso', 'latte', 'cappuccino']   
           
 lowMoney = True  
 again = True
@@ -55,37 +58,38 @@ while again:
         again= False
     elif decision == 'report':
         report()
-    ingredients = MENU[decision]["ingredients"]
-    cost = MENU[decision]["cost"]
-    dataWater -= ingredients.get('water')
-    dataCoffee -= ingredients.get('coffee')
-    dataMilk -= ingredients.get('milk')
-    if dataWater == 0:
+    elif decision in x:
+        ingredients = MENU[decision]["ingredients"]
+        cost = MENU[decision]["cost"]
+        dataWater -= ingredients.get('water')
+        dataCoffee -= ingredients.get('coffee')
+        dataMilk -= ingredients.get('milk')
+        if dataWater <= 0:
             print("Sorry, there's not enough water.")
           
-    if dataMilk == 0:
+        if dataMilk <= 0:
             print("Sorry, there's not enough milk.")
         
-    if dataCoffee == 0:
+        if dataCoffee <= 0:
             print("Sorry, there's not enough coffee.")
            
-    else:
+        else:
             resources.update({'water': dataWater, 'milk': dataMilk, 'coffee': dataCoffee})
-    print(f"Please, pay ${cost}")
-    while lowMoney:
-        quarters = int(input("how many quarters do you want to pay (0.25)?"))
-        dimes = int(input("how many dimes do you want to pay (0.10)?"))
-        nickles = int(input("how many nickles do you want to pay (0.05)?"))
-        pennies = int(input("how many pennies do you want to pay (0.01)?"))  
+        print(f"Please, pay ${cost}")
+        while lowMoney:
+            quarters = int(input("how many quarters do you want to pay (0.25)?"))
+            dimes = int(input("how many dimes do you want to pay (0.10)?"))
+            nickles = int(input("how many nickles do you want to pay (0.05)?"))
+            pennies = int(input("how many pennies do you want to pay (0.01)?"))  
 
-        paid = quarters*0.25+dimes*0.1+nickles*0.05+pennies*0.01
+            paid = quarters*0.25+dimes*0.1+nickles*0.05+pennies*0.01
     
-        if paid < cost:
-            print("Sorry, thats not enough money: Money refunded.")
+            if paid < cost:
+                print("Sorry, thats not enough money: Money refunded.")
               
-        elif paid > cost:
-            solved =round( paid - cost, 3)
-            print(f"Yout get ${solved} back." )
-            lowMoney= False
-    money += paid
+            elif paid > cost:
+                solved =round( paid - cost, 3)
+                print(f"Yout get ${solved} back." )
+                lowMoney= False
+        money += paid
     
