5a17ef381543b1c8b5933230ae1ee79253152edf
Sophie-hub-create <s.ischenko1990@gmail.com>
2021-12-21 10:14:26 +0100

    prettytable

--- Python/new/main.py
+++ Python/new/main.py
@@ -1,9 +1,22 @@
-import turtle
-import another
+# import turtle
+# import another
 
 
-timmy = turtle.Turtle()
-print(timmy)
+# timmy = turtle.Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("purple")
+# timmy.speed("slowest")
+# timmy.forward(100)
 
-my_screen = turtle.Screen()
-print(my_screen.canvheight)
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+
+
+table = PrettyTable()
+table.add_column("Pokemon Name", ['Fennekin', 'Vivllon', 'Pidgeot',"Raichu" ])
+table.add_column("Type", ["Fire", "Flying/Bug", "Flying/Normal", "Electric"])
+table.align = "l"
+print(table)
