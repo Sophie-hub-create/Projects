52bae0352065cc6a18c5b34ae9164244d38ab374
Sophie-hub-create <s.ischenko1990@gmail.com>
2021-12-26 00:16:59 +0100

    java

---
+++ Java_projects/testing.java
@@ -0,0 +1,12 @@
+class testing{
+
+
+public static void main (String args []){
+    byte a = 110;
+    short b = 3;
+    int c = 1000;
+    long d = 50000 + 10*(c +b+a);
+    System.out.println(d);
+
+}
+}
