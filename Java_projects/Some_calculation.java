ddca92a8c6356cf81651982a7b1f46f64eb60c62
Sophie-hub-create <s.ischenko1990@gmail.com>
2021-12-26 09:39:12 +0100

    ()

--- Java_projects/Some_calculation.java
+++ Java_projects/Some_calculation.java
@@ -3,7 +3,7 @@ public class Some_calculation {
 
         double a = 20.00d;
         double b = 80.00d;
-        double c = a+b*100.00d;
+        double c = (a+b)*100.00d;
         double d = c % 40.00;
         boolean e = (d == 0 )? true : false;
         System.out.println(c);
