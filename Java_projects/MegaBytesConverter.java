ec8955b8318931653b96388e815ee6693545b5cc
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-01-03 21:38:24 +0100

    megabytesconverter java

--- Java_projects/MegaBytesConverter.java
+++ Java_projects/MegaBytesConverter.java
@@ -1,3 +1,26 @@
 public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes<0){
+            System.out.println("Invalid Value");
+        }
+        else if (kiloBytes== 0){
+            System.out.println(kiloBytes+" KB = 0 MB and 0 KB");
+        }
+        else{
+            int kb = kiloBytes%1024;
+            int mb = kiloBytes/1024;
     
+            System.out.println(kiloBytes+" KB = "+mb +" MB and "+ kb+" KB");
+            }
+        }
+    
+    public static void main(String args[]) {
+
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(5000);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(20);
+        
+    }
 }
