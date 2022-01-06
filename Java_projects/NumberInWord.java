8c0bf429ad4ed22b74a4cab4776d674a197d3b06
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-01-06 11:43:31 +0100

    case

--- Java_projects/NumberInWord.java
+++ Java_projects/NumberInWord.java
@@ -8,28 +8,28 @@ public class NumberInWord {
             case 0:
                 System.out.println("ZERO");
                 break;
-                case 2:
+            case 2:
                 System.out.println("TWO");
                 break;
-                case 3:
+            case 3:
                 System.out.println("THREE");
                 break;
-                case 4:
+            case 4:
                 System.out.println("FOUR");
                 break;
-                case 5:
+            case 5:
                 System.out.println("FIVE");
                 break;
-                case 6:
+            case 6:
                 System.out.println("SIX");
                 break;
-                case 7:
+            case 7:
                 System.out.println("SEVEN");
                 break;
-                case 8:
+            case 8:
                 System.out.println("EIGHT");
                 break;
-                case 9:
+            case 9:
                 System.out.println("NINE");
                 break;
             default:
