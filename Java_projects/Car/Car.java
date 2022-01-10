9824f1d8699e75e557073c1de6d8291d8864eb41
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-01-10 13:38:43 +0100

    car

--- Java_projects/Car/Car.java
+++ Java_projects/Car/Car.java
@@ -7,6 +7,20 @@ public class Car {
     private String  model;
     private String engine;
     private String colour;
+
+    public void setColour(String colour){
+        String validColour = colour.toLowerCase();
+        if (validColour.equals("blue") || validColour.equals("black")){
+            this.colour = colour;
+        }
+        else{
+            this.colour = "Unknown";
+        }
+    }
+    
+        public String getColour(){
+            return this.colour;
+        }
     
     public void setModel(String model){
         this.model = model;
