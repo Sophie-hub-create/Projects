92451a68f430279c72955cbe06fdd80d4bcea738
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-01-10 13:59:53 +0100

    simple claculator java

--- Java_projects/Beispiele/Main.java
+++ Java_projects/Beispiele/Main.java
@@ -13,5 +13,19 @@ public class Main {
         ford.setColour("green");
         System.out.println(ford.getModel());
         System.out.println(ford.getColour());
+
+        //Beispiele Class SimpleCalculator
+
+    SimpleCalculator calculator = new SimpleCalculator();
+    calculator.setFirstNumber(5.0);
+    calculator.setSecondNumber(4);
+    System.out.println(calculator.getAdditionResult());
+    System.out.println(calculator.getSubtractionResult());
+    calculator.setFirstNumber(5.25);
+    calculator.setSecondNumber(0);
+    System.out.println(calculator.getMultiplicationResult());
+    System.out.println(calculator.getDivisionResult());
+
+
     }
 }
