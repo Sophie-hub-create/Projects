dddf3d65d6ea5b9de90a76723200e0bc891b0bc5
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-01-10 10:58:55 +0100

    car

---
+++ Java_projects/Car/Car.java
@@ -0,0 +1,18 @@
+package Car;
+
+public class Car {
+
+    private int wheels;
+    private int doors;
+    private String  model;
+    private String engine;
+    private String colour;
+    
+    public void setModel(String model){
+        this.model = model;
+    }
+    public String getModel(){
+        return this.model;
+    }
+}
+
---
+++ Java_projects/Car/Main.java
@@ -0,0 +1,10 @@
+package Car;
+
+public class Main {
+    public static void main(String[] args)  {
+       Car volvo = new Car();
+       //Car ford = new Car();
+       volvo.setModel("XC90");
+       System.out.println("Model is: "+ volvo.getModel());
+    }
+}
