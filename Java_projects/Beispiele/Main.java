8058d03297c72ce3faf0bb5ac84b17bd5a1011ee
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-01-11 17:35:44 +0100

    complex number anfang

--- Java_projects/Beispiele/Main.java
+++ Java_projects/Beispiele/Main.java
@@ -76,12 +76,14 @@ public class Main {
     System.out.println(wall.getArea());
 
     //Beispiele Point
-    Point first = new Point(6,5);
-    Point second = new Point(3,1);
-    System.out.println(first.distance());
-    System.out.println(first.distance(2,2));
-    Point point= new Point();
-    System.out.println(point.distance());
+    Point first = new Point(6, 5);
+    Point second = new Point(3, 1);
+    System.out.println("distance(0,0)= " + first.distance());
+    System.out.println("distance(second)= " + first.distance(second));
+    System.out.println("distance(2,2)= " + first.distance(2, 2));
+    Point point = new Point();
+    System.out.println("distance()= " + point.distance());
+    
 
     //Beispiele Carpet, Floor, Calculator
     Carpet carpet = new Carpet(3.5);
