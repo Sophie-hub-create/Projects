90ff5594684e3c5a6c4099de93e4f6a81c6bcbb4
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-01-14 22:23:11 +0100

    ReverseArray zuEnde machen

--- Java_projects/ReverseArray1.java
+++ Java_projects/ReverseArray1.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 public class ReverseArray1 {
     
     private static void reverse(int[] array){
-        int min=0;
+        boolean stop = false;
         int[]reverseArray = new int[array.length];
-        for(int i = 0;i<=array.length-1;i++){
-            min=array[i];
-            for (int j = array.length-1; j>=0;j--){
-                int max=array[j];
+        for (int j = array.length-1; j>=0;j--){
+        for(int i=0; i<array.length;i++){
+            reverseArray[i]=array[j];
+                }
             }
-        }
-        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(reverseArray));
+        
     }
     public static void main(String[] args) {
         int[]array = new int[]{1,2,3,4};
