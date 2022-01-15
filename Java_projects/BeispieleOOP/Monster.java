e3e3847681230bc3c0e0150bf92e0d68ba65fedb
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-01-15 22:37:32 +0100

    interfaces player, monster

---
+++ Java_projects/BeispieleOOP/Monster.java
@@ -0,0 +1,71 @@
+package BeispieleOOP;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Monster implements ISaveable {
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength){
+        this.setName(name);
+        this.setHitPoints(hitPoints);
+        this.setStrength(strength);
+        
+    }
+    @Override
+    public List<String> write() {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strength);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+        if(savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.strength = Integer.parseInt(savedValues.get(2));
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                '}';
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
