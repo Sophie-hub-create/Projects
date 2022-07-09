9957ed728937bceda2617256d4ac5279bd0e6e6d
Sophie-hub-create <s.ischenko1990@gmail.com>
2022-07-09 11:39:43 +0200

    snake

---
+++ Python/Python_turtle/day20/main.py
@@ -0,0 +1,23 @@
+from operator import truediv
+from turtle import*
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title('My Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+    
+#moving snake   
+game_is_on=True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    
+    snake.move()
+   
+screen.exitonclick()
